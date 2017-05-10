8715971d1d9fc86d97a29fab961d0e5f1895d9a5
Diego Saraiva <diego.smagnotto@gmail.com>
2017-05-10 18:21:31 -0300

    Implementação do componente Field

---
+++ angular/app/common/components/contentHeader.js
@@ -0,0 +1,13 @@
+(function(){
+    app.component('contentHeader', {
+        bindings: {
+            title: '@',
+            subTitle: '@'
+        },
+        template: `
+        <section class="content-header">
+            <h1>{{ $ctrl.title }} <small>{{ $ctrl.subTitle }}</small></h1>
+        </section>
+        `
+    });
+})();
