8d29b5833ad171094121638a82e243e7c1b4a3df
Diego Saraiva <diego.smagnotto@gmail.com>
2017-05-11 15:06:04 -0300

    Criada Factory pra tratamento das abas

--- angular/app/billingCycle/billingCycleController.js
+++ angular/app/billingCycle/billingCycleController.js
@@ -2,21 +2,37 @@
     app.controller('BillingCycleController', [
         '$http',
         'msgs',
+        'tabs',
         BillingCycleController
     ])
 
-    function BillingCycleController($http, msgs) {
+    function BillingCycleController($http, msgs, tabs) {
         const vm = this;
+
+        vm.refresh = function () {
+             const url = 'http://localhost:3003/api/billingCycles';
+
+             $http.get(url).then(function(response) {
+                vm.billingCycle = {};
+                vm.billingCycles = response.data;
+
+                tabs.show(vm, { tabList: true, tabCreate: true })
+             }).catch(function(response) {
+                 msgs.addError(response.data.errors);
+             })
+        }
 
         vm.create = function () {
             const url = 'http://localhost:3003/api/billingCycles';
 
             $http.post(url, vm.billingCycle).then(function(response) {
-                vm.billingCycle = {};
+                vm.refresh();
                 msgs.addSuccess('Registro gravado com sucesso!');
             }).catch(function(response) {
                 msgs.addError(response.data.errors);
             });
         }
-    }
+
+        vm.refresh();
+    }    
 })();
